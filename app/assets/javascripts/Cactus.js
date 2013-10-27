b3d977fc2a30163719713672f6e4425c5f58e0f5
shuzhen <sue10th@hotmail.com>
2013-10-27 16:25:04 +0800

    update collections

--- app/assets/javascripts/Cactus.js
+++ app/assets/javascripts/Cactus.js
@@ -330,28 +330,29 @@ var addOptions = {add: true, remove: false};
   //collections inheritable methods _.extend(dest, sources*)
   _.extend(Collection.prototype, Events, {
   
-   model: Model, //default
+   model: Model, 
    
+   //Initialize is an empty function by default.
    initialize: function(){}
    
-   //same
+   //The JSON representation of a Collection is an array of the models' attributes.
    toJSON: function(options) {
       return this.map(function(model){ return model.toJSON(options); });
     },
     
-	//same
+	//Proxy Backbone.sync by default.
      sync: function() {
       return Cactus.sync.apply(this, arguments); //uses cactus's sync method.
     },
     
-    //rearranged
+    //Add a model, or list of models to the set.
     add: function(models, options) {
     var addOpt = { merge:false };
     	addOpt = _.extend(addOpt, options, addOptions);
     return this.set(models, addOpt); 
     },
     
-    //rearranged. no choice. splice method is the best way to delete obj given an array
+    //splice method is the best way to delete obj given an array
     remove: function(models, options) {
 	if(!options)
 	options = {};
@@ -385,132 +386,7 @@ var addOptions = {add: true, remove: false};
 	}
     	}
     },
-    
-    //similar to model:set
-    set: function(models, options) {
-    	var singular = false;
-      options = _.defaults({}, options, setOptions);
-      if (options.parse) 
-      models = this.parse(models, options); //?
-      models = singular ? (models ? [models] : []) : _.clone(models);
-      if(toString.call(models) == '[object Array]')
-      	models = _.clone(models);
-      else{
-     singular = true;
-      if(models) models = [models];
-      else models = [];
-      } 
-      
-      var sort;
-      var at = options.at;
-      var targetModel = this.model;
-      var sortable = this.comparator && (at == null) && options.sort !== false;
-      var sortAttr = _.isString(this.comparator) ? this.comparator : null;
-      var toAdd = [], toRemove = [], modelMap = {};
-      var add = options.add, merge = options.merge, remove = options.remove;
-      var order = !sortable && add && remove ? [] : false;
 
-
-//Turn bare objects into model references, and prevent invalid models from being added.
-	  var attrs, id, model;
-      for (var i = 0,len = models.length; i< len; i++) {
-        attrs = models[i];
-        if (attrs instanceof Model) { //is a model reference
-          model = attrs;
-          id = model;
-        } else {
-          id = attrs[targetModel.prototype.idAttribute];
-        }
-
-//If a duplicate is found, prevent it from being added and optionally merge it into the existing model.
-
- 		var existing;
-        if (existing = this.get(id)) {
-          if (remove) modelMap[existing.cid] = true;
-          if (merge) {
-          	if (attrs===model) attrs = model.attributes;
-            //if (options.parse) attrs = existing.parse(attrs, options);
-            existing.set(attrs, options);
-            if (sortable && !sort && existing.hasChanged(sortAttr)) sort = true;
-          }
-          models[i] = existing;
-
-//If this is a new, valid model, push it to the toAdd list.
-        } else if (add) {
-          model = models[i] = this._prepareModel(attrs, options);
-          if (!model) continue;
-          toAdd.push(model);
-
-//Listen to added models' events, and index models for lookup by id and by cid.
-          model.on('all', this._onModelEvent, this);
-          this._byId[model.cid] = model;
-          if (model.id != null) this._byId[model.id] = model;
-        }
-        if (order) order.push(existing || model);
-      }
-
-//Remove nonexistent models if appropriate. 
-      if (remove) {
-        for (i = 0, len = this.length; i < len; ++i) {
-          if (!modelMap[(model = this.models[i]).cid]) toRemove.push(model);
-        }
-        if (toRemove.length) this.remove(toRemove, options);
-      }
-
-//See if sorting is needed, update length and splice in new models.
-      if (toAdd.length || (order && order.length)) {
-        if (sortable) sort = true;
-        this.length += toAdd.length;
-        if (at != null) {
-          for (i = 0, len = toAdd.length; i < len; i++) {
-            this.models.splice(at + i, 0, toAdd[i]);
-          }
-        } else {
-          if (order) this.models.length = 0;
-          var orderedModels = order || toAdd;
-          for (i = 0, l = orderedModels.length; i < l; i++) {
-            this.models.push(orderedModels[i]);
-          }
-        }
-      }
-
-//Silently sort the collection if appropriate.
-
-      if (sort) this.sort({silent: true});
-
-//Unless silenced, it's time to fire all appropriate add/sort events.
-
-      if (!options.silent) {
-        for (i = 0, len = toAdd.length; i < len; i++) {
-          (model = toAdd[i]).trigger('add', model, this, options);
-        }
-        if (sort || (order && order.length)) this.trigger('sort', this, options);
-      }
-
-//Return the added (or merged) model (or models).
-	if(singular) return models[0];
-	else return models;
-
-    },
-    
-    //same
-    reset: function(models, options) {
-    if(!options) options = {};
-    var len = this.models.length;
-      for (var i = 0; i < len; i++) {
-        this._removeReference(this.models[i]);
-      }
-      options.previousModels = this.models; //is never used.
-      this.length = 0;
-      this.models = [];
-      this._byId  = {};
-      var resetOpt = {silent:true};
-      	  resetOpt = _.extend(resetOpt,options); //may have problems
-      models = this.add(models, resetOpt);
-      if (!options.silent) 
-      this.trigger('reset', this, options);
-      return models;
-    },
     
     //add model to end of collection
     push: function(model, options) {
@@ -519,37 +395,28 @@ var addOptions = {add: true, remove: false};
         return model;
     },
     
+    //Remove a model from the end of the collection.
+    pop: function(options) {
+      var model = this.at(this.length - 1);
+      this.remove(model, options);
+      return model;
+    },
+    
+    //Get a model from a collection, specified by an id, a cid, or by passing in a model.
     get: function(obj) {
       if (obj == null) return void 0;
       return this._byId[obj.id] || this._byId[obj.cid] || this._byId[obj];
     },
     
-    at: function(index) {
-      return this.models[index];
-    },
     
-    where: function(attrs, first) {
-      if (_.isEmpty(attrs)) {
-      	if(first)return void 0;
-      	else return [];
-      	} 
-      return this[first ? 'find' : 'filter'](function(model) {
-        for (var key in attrs) {
-          if (attrs[key] !== model.get(key)) return false;
-        }
-        return true;
-      });
-    },
-    
-    findWhere: function(attrs) {
-      return this.where(attrs, true);
-    },
-    
-    
+/*Fetch the default set of models for this collection, 
+ *resetting the collection when they arrive. 
+ *If reset: true is passed, the response data will be passed through 
+ *the reset method instead of set.
+ */
     fetch: function(options) {
     if(options) options = _.clone(options);
     else options = {}:
-
       if (options.parse === void 0) options.parse = true;
       var success = options.success;
       var collection = this;
@@ -585,7 +452,6 @@ var addOptions = {add: true, remove: false};
       return resp;
     },
     
-    //same:(
     _prepareModel: function(attrs, options) {
       if (attrs instanceof Model) { //if attrs is already a model obj, just set its collection return
         if (!attrs.collection) 
@@ -616,7 +482,34 @@ var addOptions = {add: true, remove: false};
     'max', 'min', 'toArray', 'size', 'first', 'head', 'take', 'initial', 'rest',
     'tail', 'drop', 'last', 'without', 'difference', 'indexOf', 'shuffle',
     'lastIndexOf', 'isEmpty', 'chain'];
+    
+    var methods = ['forEach', 'each', 'map', 'collect', 'reduce', 'foldl',
+    'inject', 'reduceRight', 'foldr', 'find', 'detect', 'filter', 'select',
+    'reject', 'every', 'all', 'some', 'any', 'include', 'contains', 'invoke',
+    'max', 'min', 'toArray', 'size', 'first', 'head', 'take', 'initial', 'rest',
+    'tail', 'drop', 'last', 'without', 'difference', 'indexOf', 'shuffle',
+    'lastIndexOf', 'isEmpty', 'chain'];
 
+//Mix in each Underscore method as a proxy to Collection#models.
+  _.each(methods, function(method) {
+    Collection.prototype[method] = function() {
+      var args = slice.call(arguments);
+      args.unshift(this.models);
+      return _[method].apply(_, args);
+    };
+  });
+
+//Underscore methods that take a property name as an argument.
+  var attributeMethods = ['groupBy', 'countBy', 'sortBy'];
+//Use attributes instead of properties.
+ _.each(attributeMethods, function(method) {
+    Collection.prototype[method] = function(value, context) {
+      var iterator = _.isFunction(value) ? value : function(model) {
+        return model.get(value);
+      };
+      return _[method](this.models, iterator, context);
+    };
+  });
 var View = Cactus.View = function(options) {
     if(!options){
     	options = {};
