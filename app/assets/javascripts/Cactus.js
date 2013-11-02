d3f26140bde62792b4fa18d8fa0a4185e501f1f1
shawnleewankoon <shawnluv85@yahoo.com>
2013-11-02 14:46:26 +0800

    initialize.apply

--- app/assets/javascripts/Cactus.js
+++ app/assets/javascripts/Cactus.js
@@ -620,11 +620,25 @@ var Router = Cactus.Router = function(options) {
     if(!options){
       options = {};
     }
-    this.routes = options["routes"];
-    if(this.routes){          // bind routes
-      this.routes = _.result(this, 'routes');
-      var route, routes = _.keys(this.routes);
-      while ((route = routes.pop()) != null) {
+    this.routes = options["routes"]; //this line might be wrong
+    if(this.routes){ // bind routes
+//     _.result(object, property)
+//If the value of the named property is a function 
+//then invoke it with the object as context; otherwise, return it.
+//var object = {cheese: 'crumpets', stuff: function(){ return 'nonsense'; }};
+//_.result(object, 'cheese');
+//=> "crumpets"
+//_.result(object, 'stuff');
+//=> "nonsense"
+
+//_.keys(object)
+//Retrieve all the names of the object's properties.
+//_.keys({one: 1, two: 2, three: 3});
+//=> ["one", "two", "three"]
+      var routes = _.keys(_.result(this, 'routes'));
+      
+      while (routes.length != 0) {
+        var route=routes.pop();
         this.route(route, this.routes[route]);
       }
     }
@@ -632,30 +646,48 @@ var Router = Cactus.Router = function(options) {
 };
 
 
-//Cached regular expressions for matching named param parts and splatted parts of route strings.
+//Cached regular expressions for matching named param parts and 
+//splatted parts of route strings.
 
 var optionalParam = /\((.*?)\)/g;
-var namedParam    = /(\(\?)?:\w+/g;
-var splatParam    = /\*\w+/g;
-var escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+var namedParam = /(\(\?)?:\w+/g;
+var splatParam = /\*\w+/g;
+var escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
 
 
 //Set up all inheritable Backbone.Router properties and methods.
+//_.extend(destination, *sources)
+//Copy all of the properties in the source objects over to the destination object,
+//and return the destination object. It's in-order,
+//so the last source will override properties of the same name in previous arguments.
+//_.extend({name: 'moe'}, {age: 50});
+//=> {name: 'moe', age: 50}
 
 _.extend(Router.prototype, Events, {
 
 
-//Initialize is an empty function by default. Override it with your own initialization logic.
+//Initialize is an empty function by default.
+//Override it with your own initialization logic.
 
 initialize: function(){},
 
 
-//Manually bind a single named route to a callback. For example:this.route('search/:query/p:num', //'search', function(query, num) {
-//  ...
+//Manually bind a single named route to a callback.
+// For example:this.route('search/:query/p:num', //'search', function(query, num) {
+// ...
 //});
 
 route: function(route, name, callback) {
+  //_.isRegExp(object)
+//Returns true if object is a RegExp.
+//_.isRegExp(/moe/);
+//=> true
+
       if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+//_.isFunction(object)
+//Returns true if object is a Function.
+//_.isFunction(alert);
+//=> true
       if (_.isFunction(name)) {
         callback = name;
         name = '';
@@ -681,9 +713,8 @@ navigate: function(fragment, options) {
 },
 
 
-
-//Convert a route string into a regular expression, suitable for matching against the current //location hash.
-
+//Convert a route string into a regular expression, 
+//suitable for matching against the current //location hash.
 _routeToRegExp: function(route) {
       route = route.replace(escapeRegExp, '\\$&')
                    .replace(optionalParam, '(?:$1)?')
@@ -695,10 +726,19 @@ _routeToRegExp: function(route) {
 },
 
 
-//Given a route, and a URL fragment that it matches, return the array of extracted decoded //parameters. Empty or unmatched parameters will be treated as null to normalize //cross-browser behavior.
-
+//Given a route, and a URL fragment that it matches,
+// return the array of extracted decoded //parameters. 
+//Empty or unmatched parameters will be treated as null to 
+//normalize //cross-browser behavior.
 _extractParameters: function(route, fragment) {
       var params = route.exec(fragment).slice(1);
+      //_.map(list, iterator, [context]) Alias: collect
+//Produces a new array of values by mapping each value in list through a transformation function (iterator). If the native map method exists, it will be used instead. If list is a JavaScript object, 
+//iterator's arguments will be (value, key, list).
+//_.map([1, 2, 3], function(num){ return num * 3; });
+//=> [3, 6, 9]
+//_.map({one: 1, two: 2, three: 3}, function(num, key){ return num * 3; });
+//=> [3, 6, 9]
       return _.map(params, function(param) {
         return param ? decodeURIComponent(param) : null;
       });
