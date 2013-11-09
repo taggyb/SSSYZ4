1e890f87665528f3b14930c987f6b443fdfa7c66
Zhao Yue <zhaoyue@ubuntu.(none)>
2013-11-09 14:56:26 +0800

    delete the alert infor

--- app/assets/javascripts/Cactus.js
+++ app/assets/javascripts/Cactus.js
@@ -72,7 +72,6 @@
     // Bind an event to a `callback` function. Passing `"all"` will bind
     // the callback to all events fired.
     on: function(name, callback, context) {
-      alert("1");
       if (!eventsApi(this, 'on', name, [callback, context]) || !callback) return this;
       this._events || (this._events = {});
       var events = this._events[name] || (this._events[name] = []);
